767136c39703ea26fdf3a1d608de8d3d749b7f99
Sofia Genta <93736983+sofiigentaa@users.noreply.github.com>
2026-09-01 18:08:42 -0300

    Delete Escritorio/Candy Salon de Eventos/candy-salón-de-eventos directory

--- Escritorio/Candy Salon de Eventos/candy-salón-de-eventos/src/lib/firebase.ts
+++
@@ -1,7 +0,0 @@
-import { initializeApp, getApps } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import firebaseConfig from '../../firebase-applet-config.json';
-
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
-export const auth = getAuth(app);
-export const googleAuthProvider = new GoogleAuthProvider();
